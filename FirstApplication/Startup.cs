baae31e50041a0fae6d03b190b589ef34f626d8e
Jonas <N/A>
2019-01-07 13:43:58 +0100

    Added Configuration to Startup

--- FirstApplication/Startup.cs
+++ FirstApplication/Startup.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FirstApplication
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -24,10 +36,10 @@ namespace FirstApplication
             {
                 app.UseDeveloperExceptionPage();
             }
-            //app.UseStaticFiles();
+            app.UseStaticFiles();
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello My World!");
+                await context.Response.WriteAsync(Configuration["Message"]);
             });
         }
     }
